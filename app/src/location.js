967f188f278a6747f0283e2afa2aced6aa786988
adellanno <anthony@dellanno.co.uk>
2015-10-02 16:53:34 +0100

    browser now showing current location on map

--- app/src/location.js
+++ app/src/location.js
@@ -1,14 +1,23 @@
-// <script>
-// var x = document.getElementById("#location");
-// function getLocation() {
-//     if (navigator.geolocation) {
-//         navigator.geolocation.getCurrentPosition(showPosition);
-//     } else {
-//         x.innerHTML = "Geolocation is not supported by this browser.";
-//     }
-// }
-// function showPosition(position) {
-//     x.innerHTML = "Latitude: " + position.coords.latitude +
-//     "<br>Longitude: " + position.coords.longitude;
-// }
-// </script>
+function initMap() {
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 4,
+      center: {lat: 0, lng: 0}
+    });
+
+    var infoWindow = new google.maps.InfoWindow({map: map});
+
+    navigator.geolocation.getCurrentPosition(function(position) {
+  
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+
+      infoWindow.setContent('Found');
+      map.setCenter(pos);
+    });
+
+  }
--- app/src/weather.js
+++ app/src/weather.js
@@ -41,7 +41,16 @@ $(document).ready(function() {
       var lat = position.coords.latitude;
       var lon = position.coords.longitude;
       showLocation(lat, lon);
+
   };
+
+  // function initMap(lat, lon) {
+  //   var map = new google.maps.Map(document.getElementById('map'), {
+  //     center: {lat: lat, lng: lon},
+  //     zoom: 10
+  //   });
+  // };
+
 
   function showLocation(lat, lon) {
     $.ajax('http://api.openweathermap.org/data/2.5/weather?' + 'lat=' + lat + '&' +
@@ -54,6 +63,35 @@ $(document).ready(function() {
       }
     });
   };
-
-
+  //
+  // function initMap() {
+  //   var map = new google.maps.Map(document.getElementById('map'), {
+  //     center: {lat: lat, lng: long},
+  //     zoom: 10
+  //   });
+  // }
+//     var infoWindow = new google.maps.InfoWindow({map: map});
+//
+//     // Try HTML5 geolocation.
+//     if (navigator.geolocation) {
+//       navigator.geolocation.getCurrentPosition(function(position) {
+//         var pos = {
+//           lat: position.coords.latitude,
+//           lng: position.coords.longitude
+//         };
+//
+//         infoWindow.setPosition(pos);
+//         infoWindow.setContent('Location found.');
+//         map.setCenter(pos);
+//       }, function() {
+//         handleLocationError(true, infoWindow, map.getCenter());
+//       });
+//     } else {
+//       // Browser doesn't support Geolocation
+//       handleLocationError(false, infoWindow, map.getCenter());
+//     }
+//   }
+//
+//
+//
 });
