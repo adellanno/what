0c84900413fe6156954a64be0e6a28eeb05e8848
Anthony Dell'Anno <anthony@dellanno.co.uk>
2016-01-13 15:00:51 +0000

    modified server.js

--- server.js
+++ server.js
@@ -12,9 +12,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(root + '/index.html'));
 });
 
-var server = app.listen(3000, function () {
-var host = server.address().address;
-var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
